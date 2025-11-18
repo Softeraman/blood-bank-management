c70b2d5223e96332d4da9323eaabb705d12e0322
Softeraman <rishiamansharma457@gmail.com>
2025-11-19 04:03:24 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,3 +1,13 @@
 <?php
-$conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
+$servername = getenv('MYSQLHOST');
+$username   = getenv('MYSQLUSER');
+$password   = getenv('MYSQLPASSWORD');
+$database   = getenv('MYSQLDATABASE');
+$port       = getenv('MYSQLPORT');
+
+$conn = mysqli_connect($servername, $username, $password, $database, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
